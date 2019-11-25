b15a4884ad957abc4d13ec8d0d246efcbdd27001
Piyush2769 <startcheckingyourmail@gmail.com>
2019-11-25 12:03:02 +0530

    Paging Working +1

--- app/src/main/java/com/piyushmaheswari/paginglibrary/ItemViewAdapter.java
+++ app/src/main/java/com/piyushmaheswari/paginglibrary/ItemViewAdapter.java
@@ -2,13 +2,19 @@ package com.piyushmaheswari.paginglibrary;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.paging.PagedListAdapter;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 public class ItemViewAdapter extends PagedListAdapter<Item, ItemViewAdapter.ItemViewHolder> {
 
@@ -21,11 +27,22 @@ public class ItemViewAdapter extends PagedListAdapter<Item, ItemViewAdapter.Item
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(ctx).inflate(R.layout.recyclerview_item,parent,false);
+        return new ItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = getItem(position);
+
+        if (item != null) {
+            holder.textView.setText(item.owner.display_name);
+            Glide.with(ctx)
+                    .load(item.owner.profile_image)
+                    .into(holder.imageView);
+        }else{
+            Toast.makeText(ctx, "Item is null", Toast.LENGTH_LONG).show();
+        }
 
     }
 
@@ -45,8 +62,13 @@ public class ItemViewAdapter extends PagedListAdapter<Item, ItemViewAdapter.Item
 
     class ItemViewHolder extends RecyclerView.ViewHolder{
 
-        public ItemViewHolder(@NonNull View itemView) {
+        TextView textView;
+        ImageView imageView;
+
+        ItemViewHolder(View itemView) {
             super(itemView);
+            textView = itemView.findViewById(R.id.textViewName);
+            imageView = itemView.findViewById(R.id.imageView);
         }
     }
 }
